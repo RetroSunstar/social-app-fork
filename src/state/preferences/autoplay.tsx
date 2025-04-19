0142873e2f2ea859772bcfc0a6423d4a3f94c1f3
RetroSunstar <57507616+RetroSunstar@users.noreply.github.com>
2025-04-19 19:30:58 +0100

    Update autoplay.tsx

--- src/state/preferences/autoplay.tsx
+++ src/state/preferences/autoplay.tsx
@@ -5,14 +5,26 @@ import * as persisted from '#/state/persisted'
 type StateContext = boolean
 type SetContext = (v: boolean) => void
 
+type StateVideoContext = boolean
+type SetVideoContext = (v: boolean) => void
+
 const stateContext = React.createContext<StateContext>(
   Boolean(persisted.defaults.disableAutoplay),
 )
 const setContext = React.createContext<SetContext>((_: boolean) => {})
 
+const stateVideoContext = React.createContext<StateVideoContext>(
+  Boolean(persisted.defaults.disableVideoAutoplay),
+)
+const setVideoContext = React.createContext<SetVideoContext>((_: boolean) => {})
+
 export function Provider({children}: {children: React.ReactNode}) {
   const [state, setState] = React.useState(
     Boolean(persisted.get('disableAutoplay')),
+  )
+
+  const [videoState, setVideoState] = React.useState(
+    Boolean(persisted.get('disableVideoAutoplay')),
   )
 
   const setStateWrapped = React.useCallback(
@@ -23,20 +35,41 @@ export function Provider({children}: {children: React.ReactNode}) {
     [setState],
   )
 
+  const setVideoStateWrapped = React.useCallback(
+    (videoAutoplayDisabled: persisted.Schema['disableVideoAutoplay']) => {
+      setVideoState(Boolean(videoAutoplayDisabled))
+      persisted.write('disableVideoAutoplay', videoAutoplayDisabled)
+    },
+    [setVideoState],
+  )
+
   React.useEffect(() => {
     return persisted.onUpdate('disableAutoplay', nextDisableAutoplay => {
       setState(Boolean(nextDisableAutoplay))
     })
   }, [setStateWrapped])
 
+  React.useEffect(() => {
+    return persisted.onUpdate('disableVideoAutoplay', nextDisableVideoAutoplay => {
+      setVideoState(Boolean(nextDisableVideoAutoplay))
+    })
+  }, [setVideoStateWrapped])
+
   return (
-    <stateContext.Provider value={state}>
-      <setContext.Provider value={setStateWrapped}>
-        {children}
-      </setContext.Provider>
-    </stateContext.Provider>
+    <stateVideoContext.Provider value={videoState}>
+      <setVideoContext.Provider value={setVideoStateWrapped}>
+        <stateContext.Provider value={state}>
+          <setContext.Provider value={setStateWrapped}>
+            {children}
+          </setContext.Provider>
+        </stateContext.Provider>
+      </setVideoContext.Provider>
+    </stateVideoContext.Provider>
   )
 }
 
 export const useAutoplayDisabled = () => React.useContext(stateContext)
 export const useSetAutoplayDisabled = () => React.useContext(setContext)
+
+export const useVideoAutoplayDisabled = () => React.useContext(stateVideoContext)
+export const useSetVideoAutoplayDisabled = () => React.useContext(setVideoContext)
